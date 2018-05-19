5a56ac6e3171fb25302d108bcb30ea92ecf76e6b
Anthony <contact@anthonymayfield.com>
2018-05-18 22:48:45 -0700

    fuck

--- app.js
+++ app.js
@@ -30,6 +30,27 @@ const Promise = require('bluebird');
 const ipfilter = require('express-ipfilter').IpFilter;
 const _ = require('lodash');
 
+<<<<<<< Updated upstream
+=======
+/** Code to find errant console logs **/
+['log', 'warn', 'error'].forEach(function(method) {
+  var old = console[method];
+  console[method] = function() {
+    var stack = (new Error()).stack.split(/\n/);
+    // Chrome includes a single "Error" line, FF doesn't.
+    if (stack[0].indexOf('Error') === 0) {
+      stack = stack.slice(1);
+    }
+    var args = [].slice.apply(arguments).concat([stack[1].trim()]);
+    return old.apply(console, args);
+  };
+});
+
+const settings = require('./lib/helpers/settings');
+
+const saveAndServeFilesDirectory = settings.saveAndServeFilesDirectory;
+
+>>>>>>> Stashed changes
 /** Code for clustering, running on multiple CPUS **/
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
@@ -65,7 +86,10 @@ const settings = require('./lib/helpers/settings');
 
 const saveAndServeFilesDirectory = settings.saveAndServeFilesDirectory;
 
+<<<<<<< Updated upstream
 console.log(`SAVE AND SERVE FILES DIRECTORY: ${saveAndServeFilesDirectory}`);
+=======
+>>>>>>> Stashed changes
 
 
 if (cluster.isMaster) {
